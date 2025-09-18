ce92421cec3e8dd5b530e6b2e3c24ed7837fb30f
kia-nooshi <nooshikianoosh@gmail.com>
2025-09-17 21:12:56 -0700

    Dynamic Array: An array with a resizable capacity. If we want more room for elements we can increase capacity. AKA: Arraylist = Java      Vector = C++      Array = JavaScript      List = Python
    Advantages:
    1) Random access of elements O(1)
    2) Good locality of reference and data cache utilization
    3) Easy to insert/delete at the end
    
    Disadvantages:
    1) Wastes more memory than a linked list
    2) Shifting elements is time consuming O(n)
    3) Expanding/shrinking the array is time consuming O(n)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Can just use the arraylist object as that also works the same as a dynamic array
+        //ArrayList<String> arraylist = new ArrayList<String>();
+
+        DynamicArray dynamicArray = new DynamicArray(5);
+
+
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
+
+
+        dynamicArray.delete("F");
+        dynamicArray.delete("A");
+        dynamicArray.delete("B");
+        dynamicArray.delete("C");
+
+
+        dynamicArray.insert(0, "X");
+        dynamicArray.delete("B");
+        System.out.println(dynamicArray.search("C"));
+
+        System.out.println(dynamicArray.toString());
+        System.out.println("empty: " + dynamicArray.isEmpty());
+        System.out.println("size: " + dynamicArray.size);
+        System.out.println("capacity: " + dynamicArray.capacity);
 
     }
 }
