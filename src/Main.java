c23975dcfacd4616d235b45de79cb0e6d576f5a1
kia-nooshi <nooshikianoosh@gmail.com>
2025-09-18 21:56:56 -0700

    LinkedLists vs ArrayLists
    Accessing Any Element of Linked List is slower than an ArrayList this is because arraylists have random access of elements
    
    Removing first element is faster in LinkedList b/c of the shifts but else an arraylist is faster
    
    ArrayLists are more flexible for most Applications

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+        for(int i = 0; i< 1000000; i++){
+            linkedList.add(i);  //can add primitives because of Java Autoboxing
+            arrayList.add(i);
+        }
+
+
+        //*************************LinkedList**************************
+            startTime = System.nanoTime();
+
+            //do something
+            //linkedList.get(999999);
+            linkedList.remove(999999);
+
+            endTime = System.nanoTime();
+
+            elapsedTime = endTime - startTime;
+
+            System.out.println("LinkedList:\t " + elapsedTime + " ns");
+
+        //*************************ArrayList**************************
+            startTime = System.nanoTime();
+
+            //do something
+            //arrayList.get(999999);
+            arrayList.remove(999999 );
+
+            endTime = System.nanoTime();
+
+            elapsedTime = endTime - startTime;
+
+            System.out.println("ArrayList:\t " + elapsedTime + " ns");
 
     }
 }
