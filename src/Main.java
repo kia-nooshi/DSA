900fbbe12e72f4adc1a2a451a8accaccfd0033a4
kia-nooshi <nooshikianoosh@gmail.com>
2025-09-18 23:47:58 -0700

    Binary Search = Search algorithm that finds the position of a target value within a sorted array. Half the array is eliminated during each "step"
    runtime Complexity: O(log n)
    
    Disadvantages:
    data must be sorted prior to search
    increased implementation complexity
    
    Advantages:
    Becomes Faster the larger the dataset is
    Reduces search space by halving the interval at each step

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = new int[100];
+        int target = 42;
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = i;
+        }
+
+        //int index = Arrays.binarySearch(array, target); // cheap way is to use the built in binarySearch
+
+
+        int index = binarySearch(array, target);
+
+        if(index == -1){
+            System.out.println("Target not found");
+        }else{
+            System.out.println("Element found at: " + index);
+        }
+
+
+    }
+
+    private static int binarySearch(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high){
+            int middle = low + (high - low)/2;
+            int value = array[middle];
+            System.out.println("Middle: " + middle);
+
+            if(value < target){
+                low = middle + 1;
+            }else if(value > target){
+                high = middle - 1;
+            }else{
+                return middle; // target found
+            }
+        }
+
+
+        return -1; //target not found
     }
 }
