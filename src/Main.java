03cc153774842af5e79c8dedddebe49853d7d7a3
kia-nooshi <nooshikianoosh@gmail.com>
2025-09-21 22:43:27 -0700

    Recursion = When a thing is defined in terms of itself. Apply the result of a procedure, to a procedure. A recursive method calls itself. Can be a substitute for iteration. Divide a problem into sub-problems of the same type as the original. Commonly used with advanced sorting algorithms and navigating trees
    Advantages
    -> easier to read/write
    -> easier to debug
    
    Disadvantages
    -> sometimes slower
    -> uses more memory

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
 
+        //recursion = When a thing is defined in terms of itself.
+        //Apply the result of a procedure, to a procedure.
+        //A recursive method calls itself. Can be a substitute for iteration.
+        //Divide a problem into sub-problems of the same type as the original.
+        //Commonly used with advanced sorting algorithms and navigating trees
+
+        //Advantages
+        // -> easier to read/write
+        // -> easier to debug
+
+        //Disadvantages
+        // -> sometimes slower
+        // -> uses more memory
+
+
+        //walk(5);
+
+        System.out.println(factorial(7));
+
+        System.out.println(power(2,8));
+
+
     }
+
+    private static int power(int base, int exponent) {
+        if (exponent < 1) return 1;
+        else return base * power(base,exponent -1);
+    }
+
+    private static int factorial(int i) {
+        //base case
+        if(i < 1) return 1;
+        //recursive case
+        else return i * factorial(i -1);
+    }
+
+//    private static void walk(int steps) {
+//        for(int i = 0; i < steps; i++){  //iteration
+//            System.out.println("You take a step!");
+//        }
+//
+//        if(steps < 1) return;
+//        System.out.println("You take a step!");
+//        walk(steps -1);
+//    }
 }
