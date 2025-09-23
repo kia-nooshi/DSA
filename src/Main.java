6183af8ccadc80bbbf041e343761af16b945b7ae
kia-nooshi <nooshikianoosh@gmail.com>
2025-09-23 12:52:54 -0700

    Hashtable = A data structure that stores unique keys to values ex. <Integer, String> Each key/value pair is known as an Entry Fast insertion, look up, deletion of key/value pairs Not ideal for small data sets, great with large number sets
    Hashing = takes a key and computes an integer (formula will vary based on key & data type)
    In a Hashtable, we use the hash % capacity to calculate an index number
    
    key.hasCode() % capacity = index
    
    bucket = an indexed storage location for one or more Entries can store multiple Entries in case of a collision (linked similarly a LinkedList)
    
    collision = hash function generates the same index for more than one key
    less collisions = more efficiency
    
    Runtime Complexity: Best case O(1)
                        Worst Case O(n)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+
+        //Hashtable = A data structure that stores unique keys to values ex. <Integer, String>
+        //Each key/value pair is known as an Entry
+        //Fast insertion, look up, deletion of key/value pairs
+        //Not ideal for small data sets, great with large number sets
+
+        //Hashing = takes a key and computes an integer (formula will vary based on key & data type)
+        //In a Hashtable, we use the hash % capacity to calculate an index number
+
+
+        // key.hasCode() % capacity = index
+
+        //bucket = an indexed storage location for one or more Entries
+        // can store multiple Entries in case of a collision (linked similarly a LinkedList)
+
+        //collision = hash function generates the same index for more than one key
+        //            less collisions = more efficiency
+
+        //Runtime Complexity: Best case O(1)
+        //                    Worst Case O(n)
+
+
+
+        Hashtable<String, String> table = new Hashtable<>(21);
+        // ^ have default initial capacity of 11 and load factor of 0.75
+
+        table.put("100", "Spongebob");
+        table.put("123", "Patrick");
+        table.put("321", "Sandy");
+        table.put("555", "Squidward");
+        table.put("777", "Gary");
+
+
+
+        //table.remove(777);
+
+        for(String key: table.keySet()){
+            System.out.println(key.hashCode() % 21 + "\t"+ key+ "\t" + table.get(key));
+        }
+
+
+
+
 
     }
 }
